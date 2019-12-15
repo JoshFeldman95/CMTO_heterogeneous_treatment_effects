360fbf53214b70f23943d78526bc18df1aab02b0
Josh Feldman <joshfeldman95@gmail.com>
2019-12-15 12:07:28 -0500

    refactored in terms of s and t learners"

--- src/models.py
+++ src/models.py
@@ -1,37 +1,89 @@
 import numpy as np
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, Lasso
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.preprocessing import PolynomialFeatures
 
+from keras.models import Sequential
+from keras.layers import Dense, Activation
 
-class HeterogeneousTreatmentEffectModel:
-    def _init__(self):
-        raise NotImplementedError
+import bartpy.sklearnmodel
 
-    def fit(X, t, y):
-        raise NotImplementedError
-
-    def fit_IV(X, t, z, y):
-        raise NotImplementedError
-
-    def predict(X):
-        raise NotImplementedError
-
-    def predict_IV(X, z):
-        raise NotImplementedError
-
-class LinearRegressionHTE(HeterogeneousTreatmentEffectModel):
-    def __init__(self, *args, **kwargs):
-        self.model = LinearRegression(*args, **kwargs)
+# define base S-Learner and T-Learners
+class SLearner:
+    def __init__(self, model, *args, **kwargs):
+        self.model = model
 
     def fit(self, X, t, y):
-        X_with_treatment_interactions = np.concatenate((X, X * t), axis = 1)
-        self.model.fit(X_with_treatment_interactions, y)
+        X_with_treatment = np.concatenate((X, t.reshape(-1, 1)), axis = 1)
+        self.model.fit(X_with_treatment, y)
 
     def predict(self, X):
         # t = 0
-        X_with_treatment_0_interactions = np.concatenate((X, X * 0), axis = 1)
-        y_0 = self.model.predict(X_with_treatment_0_interactions)
+        X0 = np.concatenate((X, np.zeros((X.shape[0], 1))) , axis = 1)
+        y0 = self.model.predict(X0)
 
         # t = 1
-        X_with_treatment_1_interactions = np.concatenate((X, X * 1), axis = 1)
-        y_1 = self.model.predict(X_with_treatment_1_interactions)
-        return y_1 - y_0
+        X1 = np.concatenate((X, np.ones((X.shape[0], 1))), axis = 1)
+        y1 = self.model.predict(X1)
+        return y1 - y0
+
+class TLearner:
+    def __init__(self, model0, model1):
+        self.model0 = model0
+        self.model1 = model1
+
+    def fit(self, X, t, y):
+        X1 = X[t == 1]
+        y1 = y[t == 1]
+        self.model1.fit(X1, y1)
+
+        X0 = X[t == 0]
+        y0 = y[t == 0]
+        self.model0.fit(X0, y0)
+
+    def predict(self, X):
+        return self.model1.predict(X) - self.model0.predict(X)
+
+# implement models
+class LinearRegression_TLearner(TLearner):
+    def __init__(self, *args, **kwargs):
+        self.model0 = LinearRegression(*args, **kwargs)
+        self.model1 = LinearRegression(*args, **kwargs)
+
+class LassoWithInteractions_TLearner(TLearner):
+    def __init__(self, *args, **kwargs):
+        self.model0 = LassoWithInteractions(*args, **kwargs)
+        self.model1 = LassoWithInteractions(*args, **kwargs)
+
+class RandomForest_SLearner(SLearner):
+    def __init__(self, *args, **kwargs):
+        self.model = RandomForestRegressor(*args, **kwargs)
+
+class RandomForest_TLearner(TLearner):
+    def __init__(self, *args, **kwargs):
+        self.model0 = RandomForestRegressor(*args, **kwargs)
+        self.model1 = RandomForestRegressor(*args, **kwargs)
+
+class DecisionTree_SLearner(SLearner):
+    def __init__(self, *args, **kwargs):
+        self.model = DecisionTreeRegressor(*args, **kwargs)
+
+class DecisionTree_TLearner(TLearner):
+    def __init__(self, *args, **kwargs):
+        self.model0 = DecisionTreeRegressor(*args, **kwargs)
+        self.model1 = DecisionTreeRegressor(*args, **kwargs)
+
+# Implement Wrappers
+class LassoWithInteractions():
+    def __init__(self, degree = 2, *args, **kwargs):
+        self.poly = PolynomialFeatures(degree=degree)
+        self.model = Lasso(*args, **kwargs)
+
+    def fit(self, X, y):
+        X = self.poly.fit_transform(X)
+        self.model.fit(X, y)
+
+    def predict(self, X):
+        X = self.poly.fit_transform(X)
+        return self.model.predict(X)
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,5 +1,3 @@
-import unittest
-
 from src import models
 import pandas as pd
 
@@ -11,9 +9,9 @@ df = pd.read_csv('./data/cmto.csv', index_col = 0)
 df = df.dropna()
 
 # prep data
-T = df['received_cmto_services'].values.reshape(-1, 1)
-Z = df['treatment_group'].values.reshape(-1, 1)
-Y = (df['forecast_kravg30_p25'].values * 100).reshape(-1, 1)
+T = df['received_cmto_services'].values
+Z = df['treatment_group'].values
+Y = (df['forecast_kravg30_p25'].values * 100)
 
 X_col = ['pha', 'hoh_age', 'child_count', 'child_age', 'speaks_english',
      'born_abroad', 'working', 'homeless', 'hh_income', 'origin_pop2010',
@@ -31,11 +29,18 @@ X_train, X_test, T_train, T_test, Y_train, Y_test, Z_train, Z_test = train_test_
 
 
 def test_models():
-    model_list = [models.LinearRegressionHTE()]
+    model_list = [
+        models.LinearRegression_TLearner(),
+        models.LassoWithInteractions_TLearner(),
+        models.RandomForest_SLearner(),
+        models.RandomForest_TLearner(),
+        models.DecisionTree_SLearner(),
+        models.DecisionTree_TLearner()
+    ]
+
     for model in model_list:
         print(X_train.shape, T_train.shape, Y_train.shape)
         model.fit(X_train, T_train, Y_train)
-        assert model.predict(X_test).shape == Y_test.shape
-
-if __name__ == '__main__':
-    unittest.main()
+        HTE_pred = model.predict(X_test)
+        assert HTE_pred.shape == Y_test.shape
+        assert (HTE_pred.max() - HTE_pred.min()) > 0
